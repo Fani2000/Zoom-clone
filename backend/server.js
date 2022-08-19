cfd8c7f8bca57cfe8b68c2539d2524d2b76b8b1a
Fani2000 <42602674+Fani2000@users.noreply.github.com>
2022-08-19 14:05:04 +0200

    Finished seting up the connection and rooms between users via websocket with socketio

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,47 @@ const express = require("express");
 
 const app = express();
 const server = require("http").createServer(app);
-
 const io = require("socket.io")(server);
+
+let users = [];
 const port = 3000;
 
 io.on("connection", (socket) => {
   console.log("a user connect :d");
   socket.on("join-room", ({ roomId, userId }) => {
-    console.log("User Joined room");
-    console.log(roomId);
-    console.log(userId);
+    console.log("User Joined room", roomId, userId);
+    addUsers(userId, roomId);
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+
+    io.to(roomId).emit("all-users", getRoomUsers(roomId));
+
+    socket.on("disconnect", () => {
+      console.log(`user ${userId} has left the room.`);
+      socket.leave(roomId);
+
+      userLeave(userId);
+      io.to(roomId).emit("all-users", getRoomUsers(roomId));
+    });
   });
 });
 
 app.get("/", (req, res) => {});
+
+const userLeave = (userId) => {
+  users = users.filter((user) => user.userId != userId);
+};
+
+const getRoomUsers = (roomId) => {
+  return users.filter((user) => user.roomId === roomId);
+};
+
+const addUsers = (userId, roomId) => {
+  users.push({
+    userId,
+    roomId,
+  });
+};
 
 server.listen(port, () => {
   console.log("Listening in PORT:", port);
